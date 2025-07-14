a05343fa2c48b6a57ebcc8b6aba5e09e538e1af1
kartheekkollipara3221 <157423471+kartheekkollipara3221@users.noreply.github.com>
2025-07-14 14:39:38 -0400

    Create qlik-settings.cs

---
+++ qlik-settings.cs
@@ -0,0 +1,10 @@
+namespace QlikOrchestration.Models
+{
+    public class QlikSettings
+    {
+        public string QrsUrl { get; set; }
+        public string CertPath { get; set; }
+        public string CertPassword { get; set; }
+        public string QlikUserHeader { get; set; }
+    }
+}
